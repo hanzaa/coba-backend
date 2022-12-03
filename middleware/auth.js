1263dc1a651e812e65cc2f5bed2f980f4e7b6eb8
hanzaa <farhan.2523@gmail.com>
2022-12-03 09:11:46 +0700

    fix cookie bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,20 +5,20 @@ SECRET = process.env.SECRET
 const Auth = {
     verifyToken(req, res, next){
         // const {token = req.cookies['JWT']
-        const {token} = req.body
+        const token = req.body.token || req.cookies['JWT']
        
         if (token) {
-            // 12. Lalukan jwt verify 
-            const verified = jwt.verify(token, SECRET);
-            req.verified = verified
-            if(verified){
-                console.log("Successfully Verified");
-                return next();
-            }else{
-                // Access Denied
-                return res.status(401).send(error);
+            try {
+                // 12. Lakukan jwt verify 
+                const verified = jwt.verify(token,SECRET);
+                req.verified = verified;
+                console.log("Succesfully Verified!");
+                next()
+            } catch(err) {
+                //handle error if acces denied
+              res.status(401).send(err)
+                console.log('Invalid Token!');
             }
-
         } else {
           res.status(403).send({message: 'Youre not authenticated, please login first'})
             console.log('Youre not authenticated');
